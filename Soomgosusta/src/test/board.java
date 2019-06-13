2c2b4341a0379c3caa280d7b81c5e99d77595644
hyunjunKim <kim93j5@naver.com>
2019-06-13 15:11:33 +0900

    secoend master commit

---
+++ Soomgosusta/src/test/board.java
@@ -0,0 +1,18 @@
+package test;
+
+public class board {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	private String contents;
+}
